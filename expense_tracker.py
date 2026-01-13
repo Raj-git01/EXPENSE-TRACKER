bd35f574f6e389c906ebfeeadd30eac64dd60570
Raj <raj20063251@gmail.com>
2026-01-13 16:30:12 +0530

    updated

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,10 +1,10 @@
 import os
 
 
-def show_main():
+def show_menu():
     print("\nExpense Tracker")
     print("1. Add expense")
-    print("2. View expense")
+    print("2. View expenses")
     print("3. Exit")
 
 
@@ -12,7 +12,7 @@ def save_expenses(expenses):
     try:
         with open("expenses.txt", "w") as file:
             for expense in expenses:
-                file.write(f"{expense['description']}, {expense['amount']}\n")
+                file.write(f"{expense['description']},{expense['amount']}\n")
     except IOError:
         print("Error: Could not save expenses.")
 
@@ -28,11 +28,11 @@ def load_expenses():
             for line in file:
                 description, amount = line.strip().split(",")
                 expenses.append({
-                    "description": descriiption,
+                    "description": description,
                     "amount": float(amount)
                 })
-    except (IOError , ValueError):
-        print("Error: Could not load expenses")
+    except (IOError, ValueError):
+        print("Error: Could not load expenses.")
 
     return expenses
 
@@ -42,19 +42,19 @@ def add_expense(expenses):
         description = input("Enter expense description: ")
         amount = float(input("Enter expense amount: "))
     except ValueError:
-        print("Inavalid amount. Please enter a number.")
+        print("Invalid amount. Please enter a number.")
         return
 
     expenses.append({
         "description": description,
         "amount": amount
-        })
+    })
 
     save_expenses(expenses)
-    print("Expenses added.")
+    print("Expense added.")
 
 
-def view_expenses(exoenses):
+def view_expenses(expenses):
     if not expenses:
         print("No expenses recorded.")
         return
@@ -63,26 +63,25 @@ def view_expenses(exoenses):
 
     print("\nExpenses:")
     for index, expense in enumerate(expenses, start=1):
-        print(f"{index}. {expense['despcription']} - ${expense['amount']:.2f}")
+        print(f"{index}. {expense['description']} - ${expense['amount']:.2f}")
         total += expense["amount"]
 
     print(f"\nTotal spent: ${total:.2f}")
 
 
 def main():
-    expenses = load_expense()
+    expenses = load_expenses()
 
     while True:
         show_menu()
         choice = input("Choose an option (1-3): ")
-
 
         if choice == "1":
             add_expense(expenses)
         elif choice == "2":
             view_expenses(expenses)
         elif choice == "3":
-            print("Exciting program.")
+            print("Exiting program.")
             break
         else:
             print("Invalid choice. Please try again.")
